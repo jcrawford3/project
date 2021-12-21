7fe985e367317a1344bb74ed976cee6a604d9670
jcrawford3 <95257254+jcrawford3@users.noreply.github.com>
2021-12-21 16:03:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let weather = {
     temp: 30.2,
     humidity: 20,
   },
-  "san francisco": {
+  'san francisco': {
     temp: 20.9,
     humidity: 100,
   },
@@ -21,11 +21,19 @@ let weather = {
   },
 };
 
-city = prompt("Enter a city");
-{
-  if ((city = "paris")) {
-    alert("It is currently${weather} in Paris with a humidity of ${weather}");
-  } else {
-    alert("Sorry, we don't know the weather for this city, try going to");
-  }
+let city = prompt('Enter a city');
+city = city.toLowerCase();
+if (weather[city] !== undefined) {
+  let temperature = weather[city].temp;
+  let humidity = weather[city].humidity;
+  let celsiusTemperature = Math.round(temperature);
+  let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
+
+  alert(
+    `It is currently ${celsiusTemperature}°C (${fahrenheitTemperature}°F) in ${city} with a humidity of ${humidity}%`
+  );
+} else {
+  alert(
+    `Sorry we don't know the weather for your city, try going to https//www.google.com/search?q=weather+${city}`
+  );
 }
